327b2870585602e2b6cc7dd6aa6df37e64eb7ac1
Royal <royalbai9@gmail.com>
2023-01-20 12:44:45 -0500

    Moved nav code from App.jsx to Nav.jsx for better organization

--- joice-front/src/App.js
+++ joice-front/src/App.js
@@ -3,62 +3,20 @@ import { Routes, Route, Link } from "react-router-dom";
 import SignIn from "./Components/SignIn";
 import SignUp from "./Components/SignUp";
 import Home from './Components/Home';
+import Nav from './Components/Nav';
 import Journaling from "./Components/Journaling";
 import SavedNotes from "./Components/SavedNotes";
 import Settings from "./Components/Settings";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState("Home");
-  const mobileMenuIconRef = useRef(null);
-  const mobileMenuDropdownRef = useRef(null);
 
-  const handleMobileMenuIconClick = () => {
-    mobileMenuIconRef.current.classList.toggle("active");
-    mobileMenuDropdownRef.current.classList.toggle("active");
-  }
-
-  const handleLinkClick = (page) => {
-    mobileMenuDropdownRef.current.classList.remove("active");
-    mobileMenuIconRef.current.classList.remove("active");
-    setCurrentPage(page);
-  }
 
   return (
     <div className="App">
       <header>
         <h1>Joice</h1>
-        <div className="navHeader">
-          <button className="burgerIcon" ref={mobileMenuIconRef} onClick={handleMobileMenuIconClick}>
-            <div className="navBar"></div>
-          </button>
-          <p>{currentPage}</p>
-          <i className="fa-solid fa-magnifying-glass"></i>
-          <i className="fa-regular fa-calendar"></i>
-        </div>
+        <Nav />
       </header>
-      <nav className="mobileNav" ref={mobileMenuDropdownRef}>
-        <ul>
-          <li>
-            <i className="fa-solid fa-user"></i>
-            <div>
-              <p>Name</p>
-              <p>Email@joice.ca</p>
-            </div>
-          </li>
-          <li>
-            <i className="fa-solid fa-house-chimney"></i>
-            <Link to="/" onClick={() => handleLinkClick("Home")}>Home</Link>
-          </li>
-          <li>
-            <i className="fa-solid fa-feather"></i>
-            <Link to="/journaling" onClick={() => handleLinkClick("Create New Journal")}>Journals</Link>
-          </li>
-          <li>
-            <i className="fa-solid fa-gear"></i>
-            <Link to="/settings" onClick={() => handleLinkClick("Settings")}>Settings</Link>
-          </li>
-        </ul>
-    </nav>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
